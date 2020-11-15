de7a4d172e00459d0a6cc0a9e601172c268256ab
shradhaverma001 <shradhaverma001@gmail.com>
2020-11-15 13:07:37 +0530

    Submit button

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -1,45 +1,43 @@
 // scripts here:
 
-function openQuiz(evt, cityName, fromNext = false) {
+function openQuiz(evt, tabName, fromNext = false) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
-    if (!fromNext) { 
+    document.getElementById(tabName).style.display = "block";
+    if (!fromNext) {
         evt.currentTarget.className += " active";
     } else {
         document.getElementById("quiz2tabbutton").className += " active";
-        $('html, body').animate({scrollTop : 0},800);
+        $('html, body').animate({ scrollTop: 0 }, 800);
     }
-  }
-  
-  // Get the element with id="defaultOpen" and click on it
+}
 
-    document.getElementById("defaultOpen").click();
+// Get the element with id="defaultOpen" and click on it
+
+document.getElementById("defaultOpen").click();
 
 function submitQuiz() {
-    
-     var userPreference;
 
-    if (confirm("Are you sure to submit quizzes ?") == true) {
+    if (confirm("Are you sure you want to submit the quizzes?") == true) {
 
-        // 	document.getElementById("msg").innerHTML = userPreference;
-
-        console.log('submitted');
+        $('#submitButton').click(function () {
+            $(this).addClass('hide');
+        });
 
         // get each answer score
-        function answerScore (qName) {
+        function answerScore(qName) {
             var radiosNo = document.getElementsByName(qName);
-    
+
             for (var i = 0, length = radiosNo.length; i < length; i++) {
-                   if (radiosNo[i].checked) {
-            // do something with radiosNo
+                if (radiosNo[i].checked) {
+                    // do something with radiosNo
                     var answerValue = Number(radiosNo[i].value);
                 }
             }
@@ -49,23 +47,20 @@ function submitQuiz() {
             }
             return answerValue;
         }
-    
-    // calc score with answerScore function
-        // var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + answerScore('q4'));
-        var i, calcScore=0;
-        for(i=1;i<=20;i++){
-            calcScore=calcScore+answerScore('q'+i);
+
+        // calc score with answerScore function
+        var i, calcScore = 0;
+        for (i = 1; i <= 20; i++) {
+            calcScore = calcScore + answerScore('q' + i);
         }
-        console.log("CalcScore: " + calcScore); // it works!
-    
-    // function to return correct answer string
-        function correctAnswer (correctStringNo, qNumber) {
-            console.log("qNumber: " + qNumber);  // logs 1,2,3,4 after called below
+
+        // function to return correct answer string
+        function correctAnswer(correctStringNo, qNumber) {
             return ("The correct answer for question #" + qNumber + ": &nbsp;<strong>" +
                 (document.getElementById(correctStringNo).innerHTML) + "</strong>");
-         }
-    
-    // print correct answers only if wrong (calls correctAnswer function)
+        }
+
+        // print correct answers only if wrong (calls correctAnswer function)
         if (answerScore('q1') === 0) {
             document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctString1', 1);
         }
@@ -126,26 +121,28 @@ function submitQuiz() {
         if (answerScore('q20') === 0) {
             document.getElementById('correctAnswer20').innerHTML = correctAnswer('correctString20', 20);
         }
-    
-    // calculate "possible score" integer
+
+        // calculate "possible score" integer
         var questionCountArray = document.getElementsByClassName('question');
-    
+
         var questionCounter = 0;
         for (var i = 0, length = questionCountArray.length; i < length; i++) {
             questionCounter++;
         }
-    
-    // show score as "score/possible score"
-        var showScore = "Your Score: " + calcScore +"/" + questionCounter;
-    // if 4/4, "perfect score!"
+
+        // show score as "score/possible score"
+        var showScore = "Your Score: " + calcScore + "/" + questionCounter;
+        // if 20/20, "perfect score!"
         if (calcScore === questionCounter) {
             showScore = showScore + "&nbsp; <strong>Perfect Score!</strong>"
         };
         document.getElementById('userScore').innerHTML = showScore;
     }
-    
+
     else {
-        userPreference = "Save Canceled!";
+        $('#submitButton').click(function () {
+            $(this).removeClass('hide');
+        });
     }
 }
 
@@ -153,10 +150,10 @@ function nextQuiz(event) {
     openQuiz(event, 'Quiz2', true);
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-$('#submitButton').click(function() {
-    $(this).addClass('hide');
-});
+    $('#submitButton').click(function () {
+        $(this).addClass('hide');
+    });
 
 });
